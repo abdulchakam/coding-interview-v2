c6d68deacbae6b60d446169610c0c955350f9439
abdulchakam <abdul.chakam@ist.id>
2023-07-26 21:49:14 +0700

    Updated controller and enhance api definition

--- src/main/java/com/abdulchakam/codinginterview/CodingInterviewApplication.java
+++ src/main/java/com/abdulchakam/codinginterview/CodingInterviewApplication.java
@@ -1,9 +1,15 @@
 package com.abdulchakam.codinginterview;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(title = "Coding Interview V2", version = "1.0", description = "OpenAPI for coding interview v2 RESTFul API",
+				contact = @Contact(name = "Muhammad Abdul Chakam", email = "muhabdulchakam@gmail.com", url = "https://github.com/abdulchakam/coding-interview-v2")))
 public class CodingInterviewApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/abdulchakam/codinginterview/dog/DogController.java
+++ src/main/java/com/abdulchakam/codinginterview/dog/DogController.java
@@ -1,7 +1,6 @@
 package com.abdulchakam.codinginterview.dog;
 
 import com.abdulchakam.codinginterview.dto.BaseResponse;
-import io.swagger.annotations.ApiOperation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,28 +12,24 @@ public class DogController {
     @Autowired
     private DogService dogService;
 
-    @ApiOperation(value = "Create new dog")
     @PostMapping("/dog")
     public ResponseEntity<DogResponse> store(@Valid @RequestBody DogRequest request) {
         return dogService.storeDog(request);
     }
 
-    @ApiOperation(value = "Get all dog list")
     @GetMapping("/dog")
     public ResponseEntity<DogResponse> showAll() {
         return dogService.showAll();
     }
 
-    @ApiOperation(value = "Update dog")
     @PutMapping("/dog/{dogId}")
-    public ResponseEntity<DogResponse> update(@RequestParam Long dogId,
+    public ResponseEntity<DogResponse> update(@PathVariable Long dogId,
                                               @RequestBody DogRequest request) {
         return dogService.updateDog(dogId, request);
     }
 
-    @ApiOperation(value = "Delete dog")
     @DeleteMapping("/dog/{dogId}")
-    public ResponseEntity<BaseResponse> delete(@RequestParam Long dogId) {
+    public ResponseEntity<BaseResponse> delete(@PathVariable Long dogId) {
         return dogService.deleteDog(dogId);
     }
 }
